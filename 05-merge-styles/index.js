eebcfdc48fafe623490d32f762457bfa04786cef
vasiliyalex <vasiliyalexsandrovich@gmail.com>
2025-01-20 09:26:57 +0500

    fix: add export to 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,14 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const stylesSrc = path.join(__dirname, 'styles');
 const bundleSrc = path.join(__dirname, 'project-dist', 'bundle.css');
-const writeStream = fs.createWriteStream(bundleSrc);
 
-fs.readdir(stylesSrc, { withFileTypes: true }, (err, files) => {
-  files.forEach((file) => {
-    if (file.isFile() && path.parse(file.name).ext === '.css') {
-      const fileCssSrc = path.join(stylesSrc, file.name);
-      const readStream = fs.createReadStream(fileCssSrc, 'utf-8');
-      readStream.on('data', (chunk) => writeStream.write(chunk));
+const mergeStyles = (stylesSrc, bundleSrc) => {
+  const writeStream = fs.createWriteStream(bundleSrc);
+
+  fs.readdir(stylesSrc, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error(err);
+    } else {
+      files.forEach((file) => {
+        if (file.isFile() && path.parse(file.name).ext === '.css') {
+          const fileCssSrc = path.join(stylesSrc, file.name);
+          const readStream = fs.createReadStream(fileCssSrc, 'utf-8');
+          readStream.on('data', (chunk) => writeStream.write(chunk));
+        }
+      });
     }
   });
-});
+};
+
+mergeStyles(stylesSrc, bundleSrc);
+
+module.exports = mergeStyles;
